b20f19b5bada9787fbdda6ed29c8305ff4d53c5c
josnavch <josnavch@gmail.com>
2021-03-07 20:04:27 +0000

    avance JN

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,6 +6,7 @@ import rigoImage from "../../img/rigo-baby.jpg";
 let tareas = [];
 let i = 0;
 let tarea = "";
+let x = 0;
 
 //create your first component
 export function Home() {
@@ -16,8 +17,9 @@ export function Home() {
 	function addTodos(event, addTodo) {
 		if (event.key === "Enter") {
 			tareas.push(addTodo);
-			console.log(tareas, tareas.length);
+			//		console.log(tareas, tareas.length);
 		}
+		x = tareas.length;
 	}
 
 	const TodosHTML = tareas.map((tarea, i) => {
@@ -32,13 +34,14 @@ export function Home() {
 			<div className="text-center mt-5">
 				<ul className="list-group">
 					<input
+						id="myInput"
 						placeholder="Enter your to do's"
 						onChange={e => setName(e.target.value)}
 						onKeyPress={e => addTodos(e, value)}></input>
 					{TodosHTML}
 				</ul>
 			</div>
-			<div className="card-footer text-muted total">Total: 2 todos</div>
+			<div className="card-footer text-muted total">Total: {x} todos</div>
 		</div>
 	);
 }
